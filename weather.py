180c3b2dea27c296e7a5b99ab1efaa9a2b4f74ef
Xcal1bur <david.voigt1998@gmail.com>
2019-05-15 15:19:18 +0200

    Added docstrings and changed some colours

--- weather.py
+++ weather.py
@@ -1,14 +1,29 @@
+"""
+A command-line based client for the OpenWeatherMap API.
+"""
 import json, requests, pprint, colorama, sys, time, datetime
 
 APIkey = "857c4d16c4f5b771c726a6a0e6f60f70"
 url = "http://api.openweathermap.org/data/2.5/weather?"
 url_forecast = "http://api.openweathermap.org/data/2.5/forecast?"
 
-def getURL(Id, url, APIkey):
-    complete_url = url + "appid=" + APIkey + "&id=" + Id
+def getURL(Id, url, key):
+    """
+    Joins Id, url and key to a complete url in order to access the API
+
+    Id: id of the desirec city
+    url: Either the forecast or current weather url
+    key: Key to access the API
+    """
+    complete_url = url + "appid=" + key + "&id=" + Id
     return complete_url
 
 def search(city):
+    """
+    Reads the city.list.json file in the same directory and returns a list with all hits for a given city name which appears in the file.
+
+    city: a string containing the desired city name
+    """
     with open("".join(str(sys.path[0]) + "/city.list.json")) as file:
         data = json.load(file)
 
@@ -24,14 +39,25 @@ def search(city):
         # e.g. [{'id': 2950159, 'name': 'Berlin', 'country': 'DE', 'coord': {'lon': 13.41053, 'lat': 52.524368}}]
     return l
 
-def getData(complete_url, forecast = False):
-    if forecast:
-        return requests.get(complete_url).json()
-    else:
-        return requests.get(complete_url).json()
+def getData(complete_url):
+    """
+    Returns a dict using the requests module.
 
-def print_current_Weather(Id, url, APIkey):
-    data = getData(getURL(str(Id), url, APIkey), False)
+    complete_url: the complete url to either access the forecast or current weather API
+    forcast: Boolean to specifiy 
+    """
+    return requests.get(complete_url).json()
+    
+
+def print_current_Weather(Id, url, key):
+    """
+    Prints multiple lines containing the current weather of a given city.
+
+    Id: id of the desired city
+    url: url to access the OpenWeatherMap current weather API. Doc: https://openweathermap.org/current 
+    key: Key to access the OpenWeatherMap API
+    """
+    data = getData(getURL(str(Id), url, key))
     s = (f"[+] Weather in {data['name']}, {data['sys']['country']}:\n"
         f"[+] Date:             {time.ctime(data['dt'])}\n"
         f"[+] Description:      {data['weather'][0]['description']}\n"
@@ -41,12 +67,19 @@ def print_current_Weather(Id, url, APIkey):
         f"[+] Pressure:         {data['main']['pressure']} hPa / {round(data['main']['pressure'] / 1000, 2)} Bar\n"
         f"[+] Humidity:         {data['main']['humidity']} %\n"
         f"[+] Windspeed:        {data['wind']['speed']} m/s / {data['wind']['speed'] * 3.6} km/h\n"
-        f"[+] Cloudiness:       {data['clouds']['all']} %\n")
-    print("\n" + s)
+        f"[+] Cloudiness:       {data['clouds']['all']} %")
+    print(colorama.Fore.WHITE + s)
 
 def print_forecast(Id, url_forecast, key):
-    data = getData(getURL(str(Id), url_forecast, key), True)
-    
+    """
+    Prints multiple lines containing the forecast for a given city.
+
+    Id: id of the desired city
+    url_forecast: url to access the OpenWeatherMap forecast API. Doc: https://openweathermap.org/forecast5
+    key: Key to access the OpenWeatherMap API.
+    """
+    data = getData(getURL(str(Id), url_forecast, key))
+    print(colorama.Fore.WHITE)
     for i, e in enumerate(data['list']):
         if i == 0:
             print("\n" + str(datetime.date.today()) + ":\n")
@@ -57,6 +90,7 @@ def print_forecast(Id, url_forecast, key):
             print("")
 
 def main(url, url_forecast, key):
+    # Initializing colorama to output coloured text
     colorama.init()
     print(colorama.Fore.YELLOW,r"""                
                         |
@@ -100,9 +134,12 @@ def main(url, url_forecast, key):
                 l = search(input("[City]: "))
                 print("")
 
+                # Print the results for the search query
                 print("[*] Results:")
                 for i, e in enumerate(l, 1):
                     print(f"[{i}] Name: {e['name']}, Country: {e['country']}, ID: {e['id']}")
+
+                # Prints to result if the returned list is empty and coninues the loop
                 if l == []:
                     print(colorama.Fore.RED + "[-] No Results!")
                     continue
@@ -112,22 +149,29 @@ def main(url, url_forecast, key):
                 
                 inpt = input(":: ")
 
+                # Show current weather
                 if inpt == "1":
                     inpt = input("[City Number]: ")
                     print("")
                     print_current_Weather(l[(int(inpt) - 1)]['id'], url, key)
+                    print(colorama.Fore.GREEN)
+                # Show forecast
                 elif inpt == "2":
                     inpt = input("[City Number]: ")
                     print("")
                     print_forecast(l[int(inpt) - 1]['id'], url_forecast, key)
+                    print(colorama.Fore.GREEN)
+                # Exit to main menu
                 else:
                     continue
             # Show current weather
             elif inpt == "2":
                 print_current_Weather(search(input("[City]: "))[0]['id'], url, key)
+                print(colorama.Fore.GREEN)
             # Show forecast
             elif inpt == "3":
                 print_forecast(search(input("[City]: "))[0]['id'], url_forecast, key)
+                print(colorama.Fore.GREEN)
             # Exit
             elif inpt == "4":
                 break
@@ -136,6 +180,12 @@ def main(url, url_forecast, key):
         except KeyboardInterrupt:
             print("Exiting...")
             break
+        except ConnectionResetError:
+            print(colorama.Fore.RED + "ConnectionResetError! Please try again.")
+            continue
+        except ConnectionAbortedError:
+            print(colorama.Fore.RED + "ConncectionAbortedError! Please try again.")
+            continue
 
 if __name__ == "__main__":
     main(url, url_forecast, APIkey)
